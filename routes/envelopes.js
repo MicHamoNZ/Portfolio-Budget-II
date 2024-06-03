ef4e998d4649b29b839f3248f3b1c6a91e28c6f0
Michael Hamilton <28913158+MicHamoNZ@users.noreply.github.com>
2024-06-03 20:36:35 +1200

    back in business

---
+++ routes/envelopes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+    getEnvelopes,
+    getEnvelopeById,
+    addEnvelope,
+    updateEnvelope,
+    deleteEnvelope,
+} = require('../controllers/envelopes')
+
+router.get('/envelopes/', getEnvelopes);
+
+router.get('/envelopes/:id', getEnvelopeById);
+
+router.post('/envelopes/', addEnvelope);
+
+router.put('/envelopes/', updateEnvelope);
+
+router.delete('/envelopes/:id', deleteEnvelope);
+
+module.exports = router;
